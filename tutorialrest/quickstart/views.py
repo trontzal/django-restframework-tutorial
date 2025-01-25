3da307ee802a440193e3f1734d0201bce07fe5dd
trontzal <trontzal@gmail.com>
2025-01-25 14:34:21 +0100

    ejemplo basico terminado

--- tutorialrest/quickstart/views.py
+++ tutorialrest/quickstart/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from tutorialrest.quickstart.serializers import GroupSerializer, UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    """ endpioint que permite a los usuarios ver o editar """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """ endpoint que permite a los grupos ver o editar """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
